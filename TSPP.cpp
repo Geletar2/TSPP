71266ce26be05029649377abd730dd23d6f08bd3
Tilitchenko Dima <mail.ru>
2020-06-16 21:03:27 +0300

    create Client

--- TSPP.cpp
+++ TSPP.cpp
@@ -5,6 +5,20 @@
 #include <conio.h>
 using namespace std;
 
+class Client{	
+	public:
+		Client(){
+		}
+		string FIO;
+		Insurance _insurance;
+		
+		Client(string _FIO, Insurance * Ins);
+};
+
+Client::Client(string _FIO, Insurance * Ins): _insurance(*Ins){
+	FIO=_FIO;
+}
+
 
 int main(int argc, char** argv) {
 	return 0;
